f5f2c38ff327ac3cbfe68037a1b2e9a029164997
johnwoodill <johnwoodill@gmail.com>
2024-01-05 11:30:05 -0800

    org libs

--- libs/nlcdlibs.py
+++ libs/nlcdlibs.py
@@ -5,7 +5,7 @@ import xarray as xr
 import numpy as np
 import multiprocessing
 from tqdm import tqdm
-from math import radians, cos, sin, asin
+from math import radians, cos, sin, asin, sqrt
 
 tqdm.pandas()
 
